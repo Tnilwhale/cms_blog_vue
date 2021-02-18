d6d60960c23f3da1964efd2a892285bf1ffd4a6a
Tnilwhale <1294234770@qq.com>
2021-02-18 23:53:37 +0800

    登录验证

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from "vue-router";
 import Login from "@/views/admin/Login";
+import Index from "@/views/admin/Index";
 
 
 Vue.use(VueRouter)
@@ -11,6 +12,11 @@ const router = new VueRouter({
       path:'/login',
       name:'login',
       component:Login
+    },
+    {
+      path:'/index',
+      name:'index',
+      component:Index
     }
   ]
 })
--- src/views/admin/Login.vue
+++ src/views/admin/Login.vue
@@ -2,7 +2,7 @@
   <div class="login-page">
     <div class="login-page-bg">
       <h2>cms后台登录</h2>
-      <el-form ref="form" :model="form" label-width="80px">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="用户名" prop="userName">
           <el-input aria-placeholder="请输入用户名" v-model="form.userName"></el-input>
         </el-form-item>
@@ -10,8 +10,11 @@
           <el-input aria-placeholder="请输入密码" v-model="form.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button style="width: 100%" type="primary" @click="onSubmit">立即登录</el-button>
-          <el-button>注册</el-button>
+          <el-button style="width: 100%" type="primary" @click="submit">立即登录</el-button>
+<!--          <el-button>注册</el-button>-->
+        </el-form-item>
+        <el-form-item>
+          邵小毛嘟嘟嘟
         </el-form-item>
 
       </el-form>
@@ -27,6 +30,21 @@ export default {
       form:{
         userName:'',
         password:'',
+      },
+      rules:{
+        userName: [{required:true,message:'请输入用户名'}],
+        password: [{required:true,message:'请输入密码'}]
+      },
+      methods:{
+        submit(){
+          this.$refs['form'].validate(valid=>{
+            if(valid){
+              //执行登录操作
+              //执行页面跳转
+              this.$router.push('/index')
+            }
+          })
+        }
       }
     }
   }
@@ -37,21 +55,26 @@ export default {
 <style scoped>
 .login-page{
   height: 100%;
-  background-color: aquamarine;
+  /*background-color: aquamarine;*/
+  background: url("../../assets/image/login_bg.jpg") no-repeat;
+  background-size: cover;
 }
 .login-page-bg{
   position: absolute;
   top: 30%;
   left: 35%;
   right: 35%;
-  width: 500px;
-  bottom: 30%;
+  /*width: 500px;*/
+  /*bottom: 30%;*/
   background-color: azure;
-  border-radius: 10px;
-  padding-top: 20px;
-  padding-right: 10px;
+  border-radius: 5px;
+  padding-top: 10px;
+  padding-right: 50px;
   text-align: center;
   color: blueviolet;
+  -webkit-box-shadow: #666 0px 0px 10px;
+  -moz-box-shadow: #666 0px 0px 10px;
+  box-shadow: #666 0px 0px 10px;
 }
 .login-page-bg h2{
   margin-bottom: 50px;
